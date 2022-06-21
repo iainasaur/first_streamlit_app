0edfc39614325ee4d400a9a4dba038076e65811f
iainasaur <107482414+iainasaur@users.noreply.github.com>
2022-06-21 17:05:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ streamlit.dataframe(fruits_to_show)
 #mew section to shjow fruity vice api response
 streamlit.header('Fruity Fruit Advice!')
 
-fruit_choice = streamlit.test_input('What fruit would you like information about?', 'kiwi')
-streamlit.write('The user entered', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
