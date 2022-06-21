2949706faa5605deaf03338ead51b9766a9d4493
iainasaur <107482414+iainasaur@users.noreply.github.com>
2022-06-21 15:55:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,11 @@ streamlit.dataframe(fruits_to_show)
 
 #mew section to shjow fruity vice api response
 streamlit.header('Fruity Fruit Advice!')
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized= pandas.json_normalize(fruityvice_response.json()
+#output as a table
+streamlit.dataframe(fruityvice_normalized)                                             
